41a23b0cae1bb104a389eb90dcd7f327505bf17d
Josue Morales <molinajosue92@hotmail.com>
2023-06-03 08:24:05 -0400

    add mappings for nvimtree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,8 +1,15 @@
+local api = require('nvim-tree.api')
+
+local function opts(desc)
+  return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+end
+
 vim.keymap.set("n", "<leader>pf", ':NvimTreeFindFile<CR>') -- Find a file
 vim.keymap.set("n", "<leader>pc", ':NvimTreeCollapse<CR>') -- Close folders
-vim.keymap.set("n", "<leader>mn", require("nvim-tree.api").marks.navigate.next)
-vim.keymap.set("n", "<leader>mp", require("nvim-tree.api").marks.navigate.prev)
-vim.keymap.set("n", "<leader>ms", require("nvim-tree.api").marks.navigate.select)
+vim.keymap.set("n", "<leader>mn", api.marks.navigate.next)
+vim.keymap.set("n", "<leader>mp", api.marks.navigate.prev)
+vim.keymap.set("n", "<leader>ms", api.marks.navigate.select)
+vim.keymap.set("n", "<leader>cd", api.tree.change_root_to_node, opts('CD'))
 -- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
